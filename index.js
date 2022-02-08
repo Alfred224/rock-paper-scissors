c11e29f1a6e2764337aefe71a0325289257c9590
Alfred Arthur <alfredarthur224@gmail.com>
2022-02-08 16:59:20 +0000

    latest version of rps-ui

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-function computerPlay() {
-    const choices = ["rock", "paper", "scissors"];
+function response(arr) {
+    const choices = arr;
     return choices[Math.floor(Math.random() * choices.length)];
 };
 
+function computerPlay() {
+    //Computer's Choice
+    return response(["rock", "paper", "scissors"])
+};
+
 function playRound(playerSelection="", computerSelection="") {
+    //RPS round
     const person = playerSelection.toLowerCase(), cpu = computerSelection.toLowerCase();
     const replies = {"rock scissors": "You win, rock beats scissors.",
         "scissors paper": "You win, scissors beats paper.",
@@ -12,26 +18,85 @@ function playRound(playerSelection="", computerSelection="") {
         "paper scissors": "You lose, scissors beats paper.",
         "rock paper": "You lose, paper beats rock."};
     const result = person + " " + cpu;
-    return replies[result];
+    const reply = replies[result] || "draw";
+    return reply;
 }
 
-function game() {
-    const playerRecord = [];
-    const cpuRecord = [];
-    const result = {true: "Winner", false: "Loser"}
-    for (let i = 0; i < 5; i++) {
-        let reply;
-        do {
-            reply = playRound(prompt("Make a choice."), computerPlay());
-        } while(!reply);
-        alert(reply);
-        reply.includes("win") ? (playerRecord.push(1), cpuRecord.push(0)) : (playerRecord.push(0), cpuRecord.push(1));
+function gameplay(e) {
+    const round = document.querySelector(".round");
+    const cumscore = document.querySelector(".cumscore");
+
+    let text = e.target.className;
+    const userPlay = (text.match("rock") || text.match("paper") || text.match("scissors")).toString();
+    const CPU = computerPlay();
+    reply = playRound(userPlay, CPU);
+
+    comment.innerHTML = `<img src="./images/${userPlay}.png">` + `<img src="./images/${CPU}.png">`;
+    comment.style.transform = "scale(1)";
+    
+    if (reply.includes("win")) {
+        rndComment.innerText = response(compliments);
+        playerPoint = 1;
+        computerPoint = 0;
+    } else if (reply.includes("lose")){
+        rndComment.innerText = response(criticism);
+        playerPoint = 0;
+        computerPoint = 1;
+    } else {
+        rndComment.innerText = "Tough round";
+        playerPoint = 1;
+        computerPoint = 1;
     }
-    const playerScore = playerRecord.reduce((a, b) => a + b);
-    const cpuScore = cpuRecord.reduce((a, b) => a + b);
-    return result[playerScore > cpuScore];
+
+    playerScore += playerPoint;
+    computerScore += computerPoint;
+    round.childNodes[1].innerText = playerPoint;
+    round.childNodes[2].innerText = computerPoint;
+    cumscore.childNodes[1].innerText = playerScore;
+    cumscore.childNodes[2].innerText = computerScore;
+
+    if (playerScore >=5 || computerScore >= 5) {
+        comment.innerText = playerScore != computerScore ? result[playerScore > computerScore] : "A Tie";
+        playerScore = 0;
+        computerScore = 0;
+        selections.style.transform = "scale(0)";
+        play.innerText = "Play Again"
+        play.style.transform = "scale(1)";  
+        rndComment.innerText = "";
+    }
 }
 
-game()
+//Webpage initializers
+const gamePlay = document.querySelector("#gameplay");
+const comment = document.querySelector("#comment");
+const play = document.querySelector("#play");
+const selections = document.querySelector(".selections");
+const table = document.createElement("table");
+table.classList.add("scores");
+const thead = document.createElement("thead");
+const tbody = document.createElement("tbody");
+const rndComment = document.querySelector("#rnd-comment");
+
+play.addEventListener("click", () => {
+    comment.style.transform = "scale(0)"
+    selections.style.transform = "scale(1)";
+    play.style.transform = "scale(0)";
+    gamePlay.insertBefore(table, gamePlay.childNodes[2]);
+    thead.innerHTML = "<tr><th></th><th>Player</th><th>CPU</th></tr>";
+    tbody.innerHTML = "<tr class='round'><td>Round</td><td>0</td><td>0</td></tr><tr class='cumscore'><td>Cummulative score</td><td>0</td><td>0</td></tr>";
+    table.appendChild(thead);
+    table.appendChild(tbody);
+});
+
+//Gameplay initializers
+let playerPoint = 0;
+let computerPoint = 0;
+let playerScore = 0;
+let computerScore = 0;
+const compliments = ["Good job", "You are amazing", "Nice shot", "Impressive", "You are a natural"];
+const criticism = ["Too bad", "Better luck next time", "Don't lose hope"];
+const result = {true: "You Won", false: "You Lost"};
+const btnchoice = document.querySelectorAll(".choice");
+btnchoice.forEach(item => item.addEventListener("click", gameplay));
 
 
